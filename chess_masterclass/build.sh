1251bd677fb3f4475f5c25ff1d9aa1ed68d4154c
Tomasz Tomasik <tomasz_tomasik@yahoo.com>
2022-10-10 20:45:33 +0200

    deploying

--- chess_masterclass/build.sh
+++ chess_masterclass/build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-poetry install
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+pip install -r requirements.txt
+# python mysite/manage.py collectstatic --no-input
+python mysite/manage.py migrate
