05795d06b4900e35ef95d2969994cdf063bdea45
Tomasz Tomasik <tomasz_tomasik@yahoo.com>
2022-10-27 20:44:48 +0200

    payment fixing

--- chess_masterclass/frontend_chessmasterclass/src/constants/urlAPI.js
+++ chess_masterclass/frontend_chessmasterclass/src/constants/urlAPI.js
@@ -1,4 +1,4 @@
-const url = 'http://127.0.0.1:8000' // dev
-// const url = 'https://chess-masterclass.onrender.com' // production
+// const url = 'http://127.0.0.1:8000' // dev
+const url = 'https://chess-masterclass.onrender.com' // production
 
 export {url}
