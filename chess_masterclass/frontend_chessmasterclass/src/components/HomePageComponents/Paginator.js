838e84ecca013933a524f06f2dd6c56305f2a946
Tomasz Tomasik <tomasz_tomasik@yahoo.com>
2022-10-29 19:56:49 +0200

    paginator color change

--- chess_masterclass/frontend_chessmasterclass/src/components/HomePageComponents/Paginator.js
+++ chess_masterclass/frontend_chessmasterclass/src/components/HomePageComponents/Paginator.js
@@ -5,7 +5,16 @@ import { useLocation, useParams, Link  } from 'react-router-dom'
 const Paginator = (props) => {   
     function previusPageOnClick(e){props.previousPageHandler(e)}
     function nextPageOnClick(e){props.nextPageHandler(e)}
-    function selectPageOnClick(e){props.selectPageHandler(e)}    
+    function selectPageOnClick(e){props.selectPageHandler(e)}   
+    
+    function checkIfCurrentPage(list_index){
+        if(props.page === props.page_list[list_index]){
+            return 'black'
+        }else{
+            return 'grey'
+        }
+    }
+
   return (
     <div style={{paddingTop: '3rem'}}>
         <nav aria-label="Page navigation example">
@@ -13,10 +22,10 @@ const Paginator = (props) => {
                 <li className="page-item">
                     {props.isFirst ? <button className="page-link disabled" to="#">Previous</button> : <button className="page-link" onClick={previusPageOnClick}>Previous</button> }
                 </li>
-                <li className="page-item"><button className="page-link" data-paginator onClick={selectPageOnClick} value={props.page_list[0]}>{props.page_list[0]}</button></li>
-                <li className="page-item"><button className="page-link" data-paginator onClick={selectPageOnClick} value={props.page_list[1]}>{props.page_list[1]}</button></li>
-                <li className="page-item"><button className="page-link" data-paginator onClick={selectPageOnClick} value={props.page_list[2]}>{props.page_list[2]}</button></li>
-                <li className="page-item">
+                <li className="page-item"><button className="page-link" data-paginator onClick={selectPageOnClick} value={props.page_list[0]} style={{color:checkIfCurrentPage(0)}}>{props.page_list[0]}</button></li>
+                <li className="page-item"><button className="page-link" data-paginator onClick={selectPageOnClick} value={props.page_list[1]} style={{color:checkIfCurrentPage(1)}}>{props.page_list[1]}</button></li>
+                <li className="page-item"><button className="page-link" data-paginator onClick={selectPageOnClick} value={props.page_list[2]} style={{color:checkIfCurrentPage(2)}}>{props.page_list[2]}</button></li>
+                <li className="page-item">                    
                     {props.isLast ? <button className="page-link disabled" to="#">Next</button> : <button className="page-link" onClick={nextPageOnClick}>Next</button> }                
                 </li>
             </ul>
